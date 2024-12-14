69115321fe0d40bb23f669ffd2c6002e7fd401f1
KawsarCoder <md.kawsar.developer@gmail.com>
2024-12-15 01:28:27 +0600

    api update

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "https://docpro-app-backend.onrender.com/api",
+  baseURL: "https://docpro-app.up.railway.app/api",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
